33222359a63e0034e610fcd367d7e08c014e7614
Jgarkoo <luka81718@gmail.com>
2024-08-01 14:52:37 +0400

    update inputs

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -14,7 +14,7 @@ export class LogInComponent implements OnInit{
   
   logInForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(6)])
+    password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(30)])
   })
 
   constructor(){}
