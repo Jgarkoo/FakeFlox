42ba31ba9ea1d88028d3775c19cc2187545dd1de
Jgarkoo <luka81718@gmail.com>
2024-08-01 19:02:03 +0400

    updated sign up design

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -12,23 +12,46 @@ import { RouterLink } from '@angular/router';
 })
 export class LogInComponent implements OnInit{
   
-  logInForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(30)])
+  signIn: boolean = false;
+
+  authForm = new FormGroup({
+    name: new FormControl(''), 
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.minLength(6)),
+    confirmPassword: new FormControl('')  
   })
 
   constructor(){}
 
   ngOnInit(): void {
-    
+    this.toggleValidators(); 
   }
 
+  toggleForm() {
+    this.signIn = !this.signIn;
+    this.toggleValidators();
+  }
+
+  private toggleValidators() {
+    if (this.signIn) {
+      this.authForm.get('name')?.clearValidators();
+      this.authForm.get('confirmPassword')?.clearValidators();
+      this.authForm.get('email')?.setValidators([Validators.required, Validators.email]);
+      this.authForm.get('password')?.setValidators([Validators.required, Validators.minLength(6)]);
+    } else {
+      this.authForm.get('name')?.setValidators([Validators.required]);
+      this.authForm.get('confirmPassword')?.setValidators([Validators.required, Validators.minLength(6)]);
+      this.authForm.get('email')?.setValidators([Validators.required, Validators.email]);
+      this.authForm.get('password')?.setValidators([Validators.required, Validators.minLength(6)]);
+    }
+    this.authForm.updateValueAndValidity();  
+  }
 
   get email(){
-    return this.logInForm.controls['email'];
+    return this.authForm.controls['email'];
   }
 
   get password(){
-    return this.logInForm.controls['password'];
+    return this.authForm.controls['password'];
   }
 }
